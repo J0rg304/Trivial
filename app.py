5be3577c682698b87354a4000009191ea91757bf
Jorge <jorgecabrerarodriguez12@gmail.com>
2025-09-17 13:29:05 +0200

    Configurar puerto dinámico para Render

--- app.py
+++ app.py
@@ -18,4 +18,6 @@ def get_questions(category):
     else:
         return jsonify({"error": "Categoría no encontrada"}), 404
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
